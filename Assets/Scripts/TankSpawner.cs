0462c47dbca9ae6fea3045f7503ef97169f6140a
Oindrila Das <>
2024-11-10 04:13:35 +0530

    Tank Spawned

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _Tank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_Tank, transform.position, Quaternion.identity);//tank spawner obj is at 0,0,0
+    }
+}
